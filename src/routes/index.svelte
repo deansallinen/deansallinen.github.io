1baca3fa589ba6a8836912a738032cd9fbdb4217
Dean Sallinen <deza604@gmail.com>
2022-05-10 10:25:36 -0700

    update copy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 	<h2>Hi, I’m Dean. 👋</h2>
 	<div>
 		<p>I'm currently a Partner Success Manager at Maximizer in Vancouver, B.C.</p>
-		<p>In every new role I'm always thinking: "How can I make the experience better for the user?"</p>
+		<p>In every role I'm always thinking: "How can I use my experience, skills, and position to make the experience better for our users?"</p>
 		<p>I like to code, design, and tinker with side projects to learn new skills.  </p>
 	</div>
 </section>
