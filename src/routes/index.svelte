c37d3d2b5bbbfcfae2999166ecceeb2980a6e43a
Dean Sallinen <deza604@gmail.com>
2022-05-10 09:40:51 -0700

    remove styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,10 +5,7 @@
 </svelte:head>
 
 <section>
-	<h1>Dean Sallinen</h1>
-
 	<h2>Hi, I’m Dean. 👋</h2>
-
 </section>
 
 <style>
@@ -18,24 +15,5 @@
 		justify-content: center;
 		align-items: center;
 		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
