7b1ecde1b5ac2a0b54e593d0026f5119cac0782c
Dean Sallinen <dean@greymass.com>
2022-11-10 13:03:32 -0800

    update sveltekit, add new css framework

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  // import "../styles/reset.css";
+  import "../styles/app.css";
+</script>
+
+<header />
+
+<main class="center stack box">
+  <slot />
+</main>
+
+<footer />
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+  <title>Dean Sallinen</title>
+  <meta name="description" content="The personal website of Dean Sallinen" />
+</svelte:head>
+
+<h1>Hi, I'm <span>Dean.</span> 👋</h1>
+<p>
+  I'm currently a Web Developer at <a href="https://github.com/greymass"
+    >Greymass</a
+  >.
+</p>
+<p>
+  I'm always thinking: "How can I use my skills make the overall experience
+  better for our users?"
+</p>
+<p>
+  I like to code, design, and tinker with side projects to learn new skills.
+</p>
+
+<style>
+  h1 {
+    font-size: var(--fs-500);
+    font-weight: 800;
+  }
+  span {
+    font-size: inherit;
+    color: var(--tertiary-color);
+  }
+</style>
