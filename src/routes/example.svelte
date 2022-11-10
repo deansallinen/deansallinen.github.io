37ef011379de9b5665515332353f898cb3bab3ce
Dean Sallinen <deza604@gmail.com>
2022-11-10 11:27:32 -0800

    add css to example

--- src/routes/example.svelte
+++ src/routes/example.svelte
@@ -1,4 +1,5 @@
 <div class="hero">
+
   <video 
          loop
          muted
@@ -6,4 +7,35 @@
          preload='auto'>
     <source src='video.mp4' type='video/mp4'> Your browser does not support the video tag.
   </video>
+
+    <div class="caption">
+        <h1>This is a title</h1>
+        <h2>This is a subtitle</h2>
+    </div>
 </div>
+
+<style>
+    html, body {
+        margin: 0;
+    }
+
+    .hero {
+        display: grid;
+        grid-template-areas: "stack";
+        height: 100vh;
+        width: 100%;
+    }
+
+    .hero > * {
+        grid-area: "stack";
+    }
+
+    .hero video {
+        width: 100%;
+        object-fit: cover;
+    }
+
+    .hero.caption {
+        place-self: center;
+    }
+</style>
