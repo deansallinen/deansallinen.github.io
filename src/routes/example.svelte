92211ebc91141e7737ea4318fa9dc1a73eb7d86d
Dean Sallinen <deza604@gmail.com>
2022-11-10 11:17:58 -0800

    add example page

---
+++ src/routes/example.svelte
@@ -0,0 +1,9 @@
+<div class="hero">
+  <video 
+         loop
+         muted
+         autoplay
+         preload='auto'>
+    <source src='video.mp4' type='video/mp4'> Your browser does not support the video tag.
+  </video>
+</div>
